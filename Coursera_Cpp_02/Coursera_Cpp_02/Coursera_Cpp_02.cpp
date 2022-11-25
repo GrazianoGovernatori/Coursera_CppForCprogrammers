1b7af6ef5143849167e570821ee8e1a7d8b4284d
Graziano Governatori <ggovernatori@pdxnet.pdxeng.ch>
2022-11-25 18:53:00 +0100

    updated graph, algo

--- Coursera_Cpp_02/Coursera_Cpp_02/Coursera_Cpp_02.cpp
+++ Coursera_Cpp_02/Coursera_Cpp_02/Coursera_Cpp_02.cpp
@@ -11,9 +11,19 @@ typedef int EdgesType;
 
 //we use a conventional value to identify infinity
 constexpr EdgesType Gr_Infinity = std::numeric_limits<EdgesType>::max();
+constexpr int Gr_NoNode = std::numeric_limits<int>::max();
 
 typedef std::vector< std::vector<EdgesType> > matrix;
 
+class DijkstraListEl {
+public:
+    int nodeId;
+    int previousNodeId;
+    EdgesType edgeCost;
+    DijkstraListEl(int id, int prev, EdgesType cost) : nodeId(id), previousNodeId(prev), edgeCost(cost) {};
+};
+
+//class to store graph
 class Graph {
 public:
     Graph(int paramSize) {
@@ -36,10 +46,35 @@ public:
     void PrintValues();
     void Resize(int newSize);
     void SetElement(int nodeA, int nodeB, EdgesType edgeValue);
+    EdgesType GetEdgeDistance(int nodeA, int nodeB) { return graphMatrix[nodeA][nodeB]; }
    
 };
 
+//Dijkstra shortest-path algo class
+class Dijspa {
+public:
+    Dijspa() {
+        numberOfNodes = 0;
+        int sourceNode = 0;
+        int destNode = 0;
+    }
+    Dijspa(Graph& graphData, int graphSize) {
+        rGraph = graphData;
+        numberOfNodes = graphSize;
+    }
 
+    EdgesType CalculateSPA(int sourceNode, int destNode);
+
+private:
+    Graph rGraph = {0};
+    int numberOfNodes;
+    int sourceNode;
+    int destNode;
+};
+
+//utility functions
+void _print_open_list(std::vector<DijkstraListEl>& list);
+void _print_closed_list(std::vector<DijkstraListEl>& list);
 
 //test commit
 int main() {
@@ -55,6 +90,12 @@ int main() {
     MyGraph.SetElement(5, 8, 100);
     MyGraph.PrintValues();
     cout << endl;
+
+
+    Dijspa Algo( MyGraph, MyGraph.GetSize());
+    cout << endl << "distance btwn nodes: " << Algo.CalculateSPA(5, 3);
+
+
 
 }
 
@@ -86,5 +127,98 @@ void Graph::SetElement(int nodeA, int nodeB, EdgesType edgeValue) {
     if ((nodeA < this->size) && (nodeB < this->size)) {
         graphMatrix[nodeA][nodeB] = edgeValue;
         graphMatrix[nodeB][nodeA] = edgeValue;
+    }
+}
+
+EdgesType Dijspa::CalculateSPA(int sourceNode, int destNode) {
+    if (this->numberOfNodes < 1) {
+        return Gr_Infinity;
+    }
+    if ((sourceNode > this->numberOfNodes) || (destNode > this->numberOfNodes)) {
+        return Gr_Infinity;
+    }
+    if (sourceNode == destNode) {
+        return static_cast<EdgesType>(0);
+    }
+
+    std::vector<DijkstraListEl> closedSet;
+    std::vector<DijkstraListEl> openSet ;
+    
+    DijkstraListEl myEl(sourceNode, sourceNode, 0);
+    closedSet.push_back(myEl);
+
+    for (int iterator = 0; iterator < this->numberOfNodes; iterator++) {
+        //being symmetric, we use line zero
+        if (iterator != sourceNode) {
+            DijkstraListEl myEl(iterator, Gr_NoNode, Gr_Infinity);
+            openSet.push_back(myEl);
+        }
+    }
+
+    _print_open_list(openSet);
+    _print_closed_list(closedSet);
+
+    int minDistance;
+    int idxNodeToAdd;
+    EdgesType localDist;
+    int iterCount = 0;
+    do {
+        
+        for (int iterClosed = 0; iterClosed < closedSet.size(); iterClosed++) {
+            for (int iterOpen = 0; iterOpen < openSet.size(); iterOpen++) {
+                localDist = rGraph.GetEdgeDistance(closedSet[iterClosed].nodeId, openSet[iterOpen].nodeId);
+                if (localDist < Gr_Infinity) {
+                    if ((closedSet[iterClosed].edgeCost + localDist) < openSet[iterOpen].edgeCost) {
+                        //update open set value!
+                        openSet[iterOpen].edgeCost = closedSet[iterClosed].edgeCost + localDist;
+                        openSet[iterOpen].previousNodeId = iterClosed;
+                    }
+                }
+            }
+        }
+
+        idxNodeToAdd = Gr_NoNode;
+        minDistance = Gr_Infinity;
+        for (int iterOpen = 0; iterOpen < openSet.size(); iterOpen++) {
+            if (openSet[iterOpen].edgeCost < minDistance) {
+                idxNodeToAdd = iterOpen;
+            }
+        }
+
+        if (idxNodeToAdd != Gr_NoNode) {
+            //TODO: replace by cc
+            DijkstraListEl myEl(openSet[idxNodeToAdd].nodeId , openSet[idxNodeToAdd].previousNodeId, openSet[idxNodeToAdd].edgeCost);
+            closedSet.push_back(myEl);
+            openSet.erase(openSet.begin() + idxNodeToAdd);
+        }
+
+
+
+        cout << endl << "iteration: " << iterCount++ << endl;
+        _print_open_list(openSet);
+        _print_closed_list(closedSet);
+
+
+    } while ((openSet.size() > 0) && (idxNodeToAdd != Gr_NoNode));
+
+
+}
+
+
+//utility functions
+void _print_open_list(std::vector<DijkstraListEl> & list) {
+    cout << endl;
+    cout << "open list" << endl;
+    cout << "node\tcost" << endl;
+    for (auto x : list)  {
+        cout << x.nodeId << "\t" << x.previousNodeId << "\t" << x.edgeCost << endl;
+    }
+}
+void _print_closed_list(std::vector<DijkstraListEl>& list) {
+    cout << endl;
+    cout << "closed list" << endl;
+    cout << "node\tcost\tprev" << endl;
+    for (auto x : list) {
+        cout << x.nodeId << "\t" << x.edgeCost << "\t" << x.previousNodeId  << endl;
     }
 }
